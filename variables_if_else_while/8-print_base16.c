2cfbd396a914c2cacdaaa9433a7bf47dcd77f91b
Mtiass <6750@holbertonstudents.com>
2023-10-11 16:53:58 -0700

    task8PR2

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int bas16;
+
+	for (bas16 = 0x0; bas16 <= 0x16; bas16++)
+	{
+		putchar(bas16);
+	}
+	putchar('\n');
+	return (0);
+}
