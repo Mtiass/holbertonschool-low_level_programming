948949919551d1c8fb029157dee4096410995abf
Mtiass <6750@holbertonstudents.com>
2023-10-11 16:28:45 -0700

    task5PR2

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int hexdig;
+
+	for (hexdig = 0; hexdig <= 9; hexdig++)
+	{
+		printf("%d", hexdig);
+	}
+	return (0);
+}
