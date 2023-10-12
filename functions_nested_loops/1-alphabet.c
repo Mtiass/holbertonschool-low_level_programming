e6f0c790d6ec59f1088bcb444616e1a132fa5ff8
Mtiass <6750@holbertonstudents.com>
2023-10-12 06:54:40 -0700

    Task1Pr3

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet(void)
+	{
+		char alph;
+
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	return (0);
+}
