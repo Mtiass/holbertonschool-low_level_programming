8e735fc59a486fa4390054625d9295b52b8d0bd7
Mtiass <6750@holbertonstudents.com>
2023-10-11 17:28:01 -0700

    task9PR2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('$');
+	putchar('\n');
 	return (0);
 }
