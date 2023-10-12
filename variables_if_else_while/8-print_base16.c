893722ff6641f9e7da7556feecc1644c2df3c44d
Mtiass <6750@holbertonstudents.com>
2023-10-12 10:35:34 -0700

    Task8Pr2

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,7 +7,11 @@ int main(void)
 {
 	char bas16;
 
-	for (bas16 = 0x0; bas16 <= 0x10; bas16++)
+	for (bas16 = 48; bas16 <= 57; bas16++)
+	{
+		putchar(bas16);
+	}
+	for (bas16 = 97; bas16 = 102; bas16++)
 	{
 		putchar(bas16);
 	}
