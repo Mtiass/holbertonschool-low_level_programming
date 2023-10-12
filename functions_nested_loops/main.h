86e5f5dd1aba6e4be3c53d47110da5ef9de7c4b8
Mtiass <6750@holbertonstudents.com>
2023-10-12 06:40:44 -0700

    Task0Pr3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,12 @@
+#ifndef _main_h_
+#define _main_h_
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
+
+#endif
