88f85d46b016f60a6ad8c91b8c1d14868d7a57c5
Mtiass <6750@holbertonstudents.com>
2023-10-12 09:28:11 -0700

    Task3Pr3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks for lowercase
+ * character.
+ * @c: char to be checked.
+ * Return: 1
+ */
+int _islower(int c)
+{
+	char c;
+
+	_islower(c);
+}
