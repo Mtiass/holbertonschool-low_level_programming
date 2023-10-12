ac91a0ec6aefdc469116bac9a85cf02353189ffd
Mtiass <6750@holbertonstudents.com>
2023-10-12 09:57:32 -0700

    Task4Pr3

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: checks if
+ * lowercase ir uppercase letter.
+ * @c: char to be checked
+ * Return: 1
+ */
+int _isalpha(int c)
+{
+	_isalpha(c);
+	return (1);
+}
