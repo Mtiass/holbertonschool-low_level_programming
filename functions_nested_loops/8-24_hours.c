54fb7e7a35573165e262f5ece9aa197ef287a8fc
Mtiass <6750@holbertonstudents.com>
2023-10-13 06:22:33 -0700

    Task8PR3

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Description: "prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59."
+ * @void:
+ *
+ * Return:
+ */
+void jack_bauer(void)
+{
+	int h1, h2;
+
+	h1 = 0;
+	h2 = 0;
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 3; h2++)
+		{
+			_putchar(h2 + '0');
+		}
+		_putchar(h1 + '0');
+		_putchar(':');
+	}
+}
