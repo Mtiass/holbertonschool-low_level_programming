d4e0318e7edb64829e6dc00f88416f3be237be45
Mtiass <6750@holbertonstudents.com>
2023-10-13 09:01:31 -0700

    Task9PR3

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int add - Entry point
+ * Description: function that adds 
+ * two integers and returns the result.
+ * @A,B: int to be checked
+ *
+ * Return: int
+ */
+int add(int A, int B)
+{
+	return (A + B);
+}
