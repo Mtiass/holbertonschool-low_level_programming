f37dc90cdb21721b5a7d92e0b9f88f1c42285f11
Mtiass <6750@holbertonstudents.com>
2023-10-12 17:16:17 -0700

    Task6Pr3

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * Description:" computes the absolutei
+ * value of an integer."
+ *
+ * @A: variable -t int
+ * Return: 0
+ */
+int _abs(int A)
+{
+	int UA;
+
+	UA = (unsigned int)A;
+	_putchar(UA);
+	return (0);
+}
