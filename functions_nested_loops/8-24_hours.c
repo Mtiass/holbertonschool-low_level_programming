29534ccde5d511a25315dcb6d6bed903ea1d613b
Mtiass <6750@holbertonstudents.com>
2023-10-13 06:24:59 -0700

    Task8PR3

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 	h2 = 0;
 	for (h1 = 0; h1 <= 2; h1++)
 	{
+		_putchar(h1 + '0');
 		for (h2 = 0; h2 <= 3; h2++)
 		{
 			_putchar(h2 + '0');
 		}
-		_putchar(h1 + '0');
 		_putchar(':');
 	}
 }
