e910cdd39099d4f62b037e0501fa51fdaea0f9c4
Mtiass <6750@holbertonstudents.com>
2023-10-13 08:52:30 -0700

    Task9PR3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,13 +16,17 @@ void times_table(void)
 	{
 		for (B = 0; B <= 9; B++)
 		{
-			if (A * B / 10 == 0 && B != 0)
+			if (B != 0)
 			{
-				_putchar(' ');
-			}
-			else
-			{
+			
+				if (A * B / 10 == 0)
+				{
+					_putchar(' ');
+				}	
+				else
+				{
 				_putchar(((A * B) / 10) + '0');
+				}
 			}
 			_putchar(((A * B) % 10) + '0');
 			if (B != 9)
