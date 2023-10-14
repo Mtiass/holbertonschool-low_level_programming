df4ed8010753f0817256ac2793af8d622e6989d9
Mtiass <6750@holbertonstudents.com>
2023-10-14 07:57:03 -0700

    Pr5task4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Description:"prints the numbers, from 0 to 9,
+ * followed by a new line."
+ * Return:
+ */
+void print_most_numbers(void);
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		while (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
