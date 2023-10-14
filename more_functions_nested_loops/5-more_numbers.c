9bc6e7b39c98d6c836365d41a6bf627c13f1046a
Mtiass <6750@holbertonstudents.com>
2023-10-14 08:12:10 -0700

    Pr5task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Description:"prints 10 times the numbers, from
+ * 0 to 14, followed by a new line."
+ * Return:
+ */
+void more_numbers(void)
+{
+	int newl, num;
+
+	newl = 0;
+	while (newl < 10)
+	{
+		for (num = '0'; num <= '14'; num++)
+		{
+			_putchar(num + '0');
+		}
+		_putchar('\n');
+		newl++;
+	}
+}
