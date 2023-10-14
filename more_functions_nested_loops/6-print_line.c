04b744040576da0d5656fad8cb365c22eeb7211c
Mtiass <6750@holbertonstudents.com>
2023-10-14 10:42:52 -0700

    Pr5task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: function that draws a straight
+ * line in the terminal.
+ * @n: int to be checked.
+ * Return:
+ */
+void print_line(int n)
+{
+	n = '_';
+	if (n > 0)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
