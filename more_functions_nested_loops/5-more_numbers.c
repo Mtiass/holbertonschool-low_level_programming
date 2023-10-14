d135235ecb34f86ed060a74fa0ae1c0fa26c8034
Mtiass <6750@holbertonstudents.com>
2023-10-14 08:41:47 -0700

    Pr5task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,10 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			_putchar(num % 10 + '0');
-		}
-		if (num >= 10 && num <= 14)
-		{
-			_putchar(num / 10 + '0');
+			if (num >= 10 && num <= 14)
+			{
+				_putchar(num / 10 + '0');
+			}
 		}
 		_putchar('\n');
 		newl++;
