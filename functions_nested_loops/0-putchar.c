4d7b636352e5d904e4e72ecc1b820a7efdb30b65
Mtiass <6750@holbertonstudents.com>
2023-10-14 06:27:49 -0700

    correctingtask0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,10 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	puts("_putchar");
 	return (0);
 }
