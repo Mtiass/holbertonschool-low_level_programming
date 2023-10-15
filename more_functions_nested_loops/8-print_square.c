52f004121505b8d88f56a1da9b4c6f0ced16992e
Mtiass <6750@holbertonstudents.com>
2023-10-15 12:53:45 -0700

    Pr5Task8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h'
+
+/**
+ * print_square - Entry point
+ * Description:"function that prints a square,
+ * followed by a new line."
+ * @size: int to be checked
+ * Return:
+ */
+void print_square(int size)
+{
+	int m, n;
+
+	m = n = 0;
+	while (m < size)
+	{
+		while (n < size)
+		{
+			_putchar(35);
+			_putchar('\n');
+			n++;
+		}
+		_putchar(35);
+		m++;
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	
