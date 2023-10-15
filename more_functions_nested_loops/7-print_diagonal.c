45f9f8dd66d8b417bb3c49946125133f0a7d9eb4
Mtiass <6750@holbertonstudents.com>
2023-10-14 17:28:39 -0700

    Pr5Task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * Description:"function that draws a diagonal
+ * line on the terminal."
+ * @n: int to be checked.
+ * Return:
+ */
+void print_diagonal(int n)
+{
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		if (n > 0)
+		{
+			_putchar(92);
+		}
+	}
+	_putchar('\n');
+}
+
