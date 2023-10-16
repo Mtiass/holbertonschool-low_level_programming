519eeb9c04afdf1b118b984970ae2f44aef3de41
Mtiass <6750@holbertonstudents.com>
2023-10-16 10:55:51 -0700

    Pr5Task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,14 @@ void print_triangle(int size)
 
 	for (a = 0; a < size; a++)
 	{
+		for (b = 0; b < a; b--)
+		{
+			_putchar(' ');
+		}
 		for (b = 0; b < a; b++)
 		{
 			_putchar(35);
 		}
-		_putchar(' ');
 		_putchar('\n');
 	}
 	if (size <= 0)
