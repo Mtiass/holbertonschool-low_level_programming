b543faaacfb4d4402a2f86d2cd3d835a52e658f1
Mtiass <6750@holbertonstudents.com>
2023-10-16 08:26:38 -0700

    Pr5Task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		if else ((m % 5) == 0)
+		else if ((m % 5) == 0)
 		{
 			printf("buzz ");
 		}
-		if else ((m % 3 == 0) && (m % 5 == 0))
+		else if ((m % 3 == 0) && (m % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
