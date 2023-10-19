77987a02d51fb25e3ecfc5a00172d475b2489a05
Mtiass <MDLSproff@gmail.com>
2023-10-18 18:38:24 -0700

    Pr5Ts4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * Description:"In README.md file"
+ * @s:"pointer type char"
+ * Return:
+ */
+void print_rev(char *s)
+{
+	int revst;
+
+	revst = '\0';
+	while (s[revst] <= '\0')
+	{
+		if (s[revst] < '\0')
+		{
+			_putchar(s[revst]);
+		}
+		revst--;
+	}
+	_putchar('\n');
+}
