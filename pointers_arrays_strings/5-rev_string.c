bd7625901ee07a466b3dac4de10a9b4720ee67be
Mtiass <MDLSproff@gmail.com>
2023-10-19 06:46:40 -0700

    PrPAS-T5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - Entry point
+ * Description:"In README.md file"
+ * @s:"pointer type char"
+ * Return:
+ */
+void rev_string(char *s)
+{
+	int revst;
+
+	revst = strlen(s) - 1;
+	while (revst >= 0)
+	{
+		_putchar(s[revst]);
+		revst--;
+	}
+	_putchar('\n');
+}
