a8200c3c211ce29db6de4e5ce830226eedf4c745
Mtiass <MDLSproff@gmail.com>
2023-10-19 07:07:19 -0700

    PrPAS-T5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,21 +9,8 @@
  */
 void rev_string(char *s)
 {
-	int prnst;
 	int revst;
-	
-	prnst = 0;
+
 	revst = strlen(s) - 1;
-	while (s[prnst] != '\0')
-	{
-		_putchar(s[prnst]);
-		prnst++;
-	}
-	_putchar('\n');
-	while (revst >= 0)
-	{
-		_putchar(s[revst]);
-		revst--;
-	}
 	_putchar('\n');
 }
