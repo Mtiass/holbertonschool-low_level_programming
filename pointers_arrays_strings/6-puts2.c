71c782fe47cca6a7d4dd88c461c6c97b5c2e7383
Mtiass <MDLSproff@gmail.com>
2023-10-19 08:16:39 -0700

    PrPAS-T6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _puts - Entry point
+ * puts2 - Entry point
  * Descritpion:"In README.md"
  * @str:"pointer variable type char."
  * Return:
  */
-void _puts(char *str)
+void puts2(char *str)
 {
 	int st;
 
 	st = 0;
 	while (str[st] != '\0')
 	{
+		_putchar(str[st]);
 		if (str[st] != '\0')
 		{
 			st++;
 		}
-		_putchar(str[st]);
                 st++;
 	}
 	_putchar('\n');
