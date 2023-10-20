2305843ddb76588eb596e5cb109a4881d7000dd4
Mtiass <MDLSproff@gmail.com>
2023-10-20 05:26:48 -0700

    PrPAS-T8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * Description:"In README.md file."
+ * @a:variable pointer.
+ * @n:viariable type int
+ * Return:
+ */
+void print_array(int *a, int n)
+{
+	for (n = 0; n != '\0'; n++)
+	{
+		printf("%d, ", a[n])
+	}
+	printf("\n");
+}
