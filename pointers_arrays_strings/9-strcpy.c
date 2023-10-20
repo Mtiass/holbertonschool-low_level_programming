a7e76789e08d7bff04fd060a42a881baa55bd6dc
Mtiass <MDLSproff@gmail.com>
2023-10-20 06:27:23 -0700

    PrPAS-T8

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Entry point
+ * Description:"In README.md file"
+ * @*dest: 
+ * @*src:
+ * Return: type char.
+ */
+char *_strcpy(char *dest, char *src)
+{	
+	char ask;
+
+	*dest = *src;
+	ask = *dest;
+	return (ask);
+}
