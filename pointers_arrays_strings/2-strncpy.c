b57e0fedcc10fd450843f1cd8d24bb9a2d37514d
Mtiass <MDLSproff@gmail.com>
2023-10-22 06:19:40 -0700

    PMPAS-T2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int con;
 
 	con = 0;
-	while (con < n)
+	while (con < n && src[con] != '\0')
 	{
 		dest[con] = src[con];
 		con++;
 	}
-	dest[con] = '\0';
+	while (con < n)
+	{
+		dest[con] = '\0';
+	}
 	return (dest);
 }
