277d71617e2845608874be4ed997462c1fb91d71
Mtiass <MDLSproff@gmail.com>
2023-10-22 05:31:18 -0700

    PMPAS-T2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * Description:"In README.md file."
+ * @dest:first pointer param.
+ * @src:second pointer param.
+ * @n:variable int type.
+ * Return: pointer char type.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int con;
+
+	for (con = 0; src[con] != n; con++)
+	{
+		dest[con] = src[con]
+	}
+	dest[con + 1] = '\0';
+	return (dest);
+}
