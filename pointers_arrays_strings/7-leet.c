4ec67de342b6b49e443af5ccf046dd58e2f206ab
Mtiass <MDLSproff@gmail.com>
2023-10-23 07:24:38 -0700

    PrMPAS-T7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,21 +7,23 @@
  * @str: char pointer as parameter.
  * Return: char pointer.
  */
-char *leet(char *)
+char *leet(char *str)
 {
 	char *str;
 	int con, con2;
 	a[10];
        	b[10];
 
+	a[] = {a, A, e, E, o, O, t, T, l, L};
+	b[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 	for (con = 0; str[con] != '\0'; con++)
 	{
 		for (con2 = 0; a[con2] < 10; con2++)
 		{
-			if (str[con] = a[con2])
+			if (str[con] == a[con2])
 			{
 				a[] = b[];
-				str[con] = a[con2];
+				str[] = a[];
 			}
 		}
 	}
