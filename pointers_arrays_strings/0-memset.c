46f901a37f5717a7fdcc9728ecbd5ceae8bb4cdd
Mtiass <MDLSproff@gmail.com>
2023-10-23 16:54:44 -0700

    EpasT0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * Description:"function that fills memory
+ * with a constant byte."
+ * @s: string to be converted.
+ * @b: char var.
+ * @n: unsigned int number limit
+ * Return: *s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int con;
+
+	for (con = 0; s[con] < n; con++)
+	{
+		*s = b;
+	}
+	return (s);
+}
