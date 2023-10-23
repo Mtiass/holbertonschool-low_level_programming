7a4dfe390e7803982663b9efa223ddcb80d5b2e1
Mtiass <MDLSproff@gmail.com>
2023-10-23 07:08:54 -0700

    PrMPAS-T7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * Description:"Write a function that encodes
+ * a string into 1337."
+ * @str: char pointer as parameter.
+ * Return: char pointer.
+ */
+char *leet(char *)
+{
+	char *str;
+	int con, con2;
+	a[10];
+       	b[10];
+
+	for (con = 0; str[con] != '\0'; con++)
+	{
+		for (con2 = 0; a[con2] < 10; con2++)
+		{
+			if (str[con] = a[con2])
+			{
+				a[] = b[];
+				str[con] = a[con2];
+			}
+		}
+	}
+}	
