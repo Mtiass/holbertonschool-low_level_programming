1cd17d4205f80be12d4ab11c15286d249fb851cb
Mtiass <MDLSproff@gmail.com>
2023-10-23 18:48:51 -0700

    EpasT1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * Description:"function that locates a character
+ * in a string."
+ * @s: string to be checked.
+ * @c: char delimiter.
+ * Return: string initialized by c.
+ */
+char *_strchr(char *s, char c)
+{
+	int con;
+
+	for (con = 0; s[con] != '\0'; con++)
+	{
+		if (s[con] = 'c')
+		{
+
