c9c88b7efef4a64329805855a9d265d3f2c207ff
Mtiass <MDLSproff@gmail.com>
2023-10-23 17:18:30 -0700

    EpasT1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ * Description:"function that copies memory area"
+ * @dest: string to be returned
+ * @src: string to be copied
+ * @n: unsigned int limiter
+ * Return: a string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int con;
+
+	con = 0;
+	while (con < n && src[con] != '\0')
+	{
+		dest[con] = src[con];
+		con++;
+	}
+	while (con < n)
+	{
+		dest[con] = '\0';
+	}
+	return (dest);
+}
