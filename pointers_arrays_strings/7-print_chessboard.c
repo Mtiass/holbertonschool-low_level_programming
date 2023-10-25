8ad37763781c6747efaa7958aae3ca384a3f7800
Mtiass <MDLSproff@gmail.com>
2023-10-25 08:45:08 -0700

    PrEmPAS-T6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -10,11 +10,13 @@ void print_chessboard(char (*a)[8])
 {
 	int c1, c2;
 
-	for (c1 = 0; a[c1] < 8; c1++)
+	for (c1 = 0; c1 < 8; c1++)
 	{
-		for (c2 = 0; a[c2] < 8; c2++)
+		for (c2 = 0; c2 < 8; c2++)
 		{
+
 			_putchar(a[c1][c2]);
 		}
+		_putchar('\n');
 	}
 }
