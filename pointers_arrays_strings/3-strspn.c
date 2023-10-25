d74accd8470d01c1878f064c5f461aa0b2c4628c
Mtiass <MDLSproff@gmail.com>
2023-10-25 05:24:27 -0700

    PrEmPAS-T3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int len = 0;
 	int con1, con2;	
 
-	for (con1; s[con1] != '\0'; con1)
+	for (con1; s[con1] != '\0'; con1++)
 	{	
 		for (con2 = 0; accept[con2] != '\0'; con2++)
 		{
