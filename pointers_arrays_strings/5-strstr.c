8873c95e21029abf8c1a7a4bd69a4432a0d19c7f
Mtiass <MDLSproff@gmail.com>
2023-10-25 06:37:21 -0700

    PrEmPAS-T5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * Description:"function that locates a
+ * substring."
+ * @haystack:string to be searched. 
+ * @needle:substring to find occurrence.
+ * Return: char string.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int c1, c2;
+
+	for (c1 = 0; haystack[c1] != '\0'; c1++)
+	{
+		for (c2 = 0; needle[c2] != '\0'; c2++)
+		{
+			if (haystack[c1] == needle[c2])
+			{
+				return (needle);
+			}
+		}
+	}
+	return ('\0');
+}
