96c030d3a86457e9044aa8e2fe659b0dcc31c123
Mtiass <MDLSproff@gmail.com>
2023-10-26 08:16:33 -0700

    PrRecursionT1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,16 +11,8 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		s++;
-	}
-	s--;
-	if (*s != 0)
-	{
-		_putchar(*s);
-		s--;
 		_print_rev_recursion(s);
-	}
-	else
-	{
-		_putchar('\n');
+		s--;
+		_putchar(s);
 	}
 }
