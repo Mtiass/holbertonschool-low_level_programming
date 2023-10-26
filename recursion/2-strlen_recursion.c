5caf47af06c38377922c9e38f82c4c546561d1d2
Mtiass <MDLSproff@gmail.com>
2023-10-26 08:53:56 -0700

    PrRecursionT2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ * Description:"function that returns the
+ * length of a string."
+ * @s: string to search it's lenght.
+ * Return: an integer.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (_strlen_recursion(s) + 1);
+	else 
+	{
+		return (0);
+	}
+}	
