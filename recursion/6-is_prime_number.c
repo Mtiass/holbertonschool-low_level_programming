f7c740edf2a3315f55b295b20e2012259263cc66
Mtiass <MDLSproff@gmail.com>
2023-10-26 17:48:21 -0700

    6thTaskRecursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_prime_number - Entry point
+ * Description:function that returns 1 if the
+ * input integer is a prime number, otherwise
+ * return 0.
+ * @n:integer.
+ * Return: 1 if true 0 if false.
+ */
+int is_prime_number(int n)
+{
+	if (((n % 2) == 0) && n != 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
