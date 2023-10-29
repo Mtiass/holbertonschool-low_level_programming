71b4989a5c7dd73f4ec90fccd9076a2ea43291c2
Mtiass <MDLSproff@gmail.com>
2023-10-29 09:56:25 -0700

    argc, argv T-4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description:program that adds positive
+ * numbers.
+ * @argc: argument counter.
+ * @argv:argument vector(array) of strings.
+ * Return:integer 0.
+ */
+int main(int argc, char **argv)
+{
+	int sum, con, pos = 0;
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (con = 1; con < argc; con++)
+	{
+		while (argv[con][pos] != '\0')
+		{
+			if (argv[con][pos] < '0' || argv[con][pos] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			pos++;
+		}
+		if (atoi(argv[con]) > 0)
+		{
+			sum += atoi(argv[con]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}	
