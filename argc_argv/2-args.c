29cd94cd9dd65bada1fd947a8d4da42ed0dffe95
Mtiass <MDLSproff@gmail.com>
2023-10-29 05:46:22 -0700

    argc, argv T-2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:"program that prints all arguments
+ * it receives."
+ * @argc:argument counter.
+ * @argv:argument vector(array) of strings.
+ * Return:integer 0.
+ */
+int main(int argc, char **argv)
+{
+	int con;
+	if (argv[con] != argv[argc])
+	{
+		for (con = 0; con < argc; con++)
+		{
+			printf("%s\n", argv[con]);
+		}
+	}
+	return (0);
+}
