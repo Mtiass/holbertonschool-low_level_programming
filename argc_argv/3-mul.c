28ccdbc47065c7cc42a11f8e16de2e1c627b2c68
Mtiass <MDLSproff@gmail.com>
2023-10-29 06:28:52 -0700

    argc, argv T-3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description:"program that multiplies two
+ * numbers."
+ * @argc:argument counter.
+ * @argv:argument vector(array) of strings.
+ * Retrun: integer 0.
+ */
+int main(int argc, char **argv)
+{
+	int con, mult = 1;
+	char war[] = "Error";
+
+	if (argc > 2)
+	{
+		for (con = 1; con < argc; con++)
+		{
+			mult *= atoi(argv[con]);
+		}
+		printf("%d", mult);
+	}
+	else
+	{
+		printf("%s", war);
+		return (1);
+	}
+	return (0);
+}
