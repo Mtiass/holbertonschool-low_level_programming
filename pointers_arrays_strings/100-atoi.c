41076d3c0a8acf0bbe00e74c8b77fff941a1886f
Mtiass <MDLSproff@gmail.com>
2023-10-29 17:18:16 -0700

    Pointers, arrays and strings T10-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,35 @@
  */
 int _atoi(char *s)
 {
-	int con, res, sign;
+	int con, res, sign, con2;
 
+	con = 0;
 	res = 0;
 	sign = 1;
-	while (s[con] == '+' || s[con] == '-')
+	con2 = 0;
+	while (s[con] == '-' || s[con] == '+')
 	{
 		if (s[con] == '-')
 		{
-			sign = -sign;
+			sign *= -1;
 		}
 		con++;
 	}
-	for (con = 0; s[con] != '\0'; con++)
+	for (; s[con] != '\0'; con++)
 	{
 		if (s[con] >= '0' && s[con] <= '9')
 		{
 			res = res * 10 + (s[con] - '0');
+			con2++;
 		}
 		else
 		{
 			break;
 		}
+	}
+	if (con2 == 0)
+	{
+		return (0);
 	}
 	return (res * sign);
 }
