91dc9b80792900aaf9d5d184bc32fe050dd71484
Mtiass <MDLSproff@gmail.com>
2023-10-30 10:07:02 -0700

    PrMPAS-T06

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - Entry point
+ * Description:"function that capitalizes all
+ * words of a string."
+ * @st: string to be capitalized.
+ * Return: a char pointer to a string.
+ */
+char *cap_string(char *st)
+{
+	int con;
+
+	con = 0;
+	while (st[con])
+	{
+		while (!(st[con] >= 'a' && st[con] <= 'z'))
+		{
+			con++;
+		}
+		if (st[con - 1] == ' ' || st[con - 1] == '\t' ||
+		st[con - 1] == '\n' || st[con - 1] == ',' ||
+		st[con - 1] == ';' || st[con - 1] == '.' ||
+		st[con - 1] == '!' || st[con - 1] == '?' ||
+		st[con - 1] == '"' || st[con - 1] == '(' ||
+		st[con - 1] == ')' || st[con - 1] == '{' ||
+		st[con - 1] == '}')
+		{
+			st[con] -= 32;
+			con++;
+		}
+	}
+	return (st);
+}
