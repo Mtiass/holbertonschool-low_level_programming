11ce24690688626fd90f754a73bdd2345c47ebd8
Mtiass <MDLSproff@gmail.com>
2023-10-30 11:57:22 -0700

    PrEmPAS-T8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,11 +17,11 @@ void print_diagsums(int *a, int size)
 	add2 = 0;
 	for (con = 0; con < size; con++)
 	{
-		add += *((a + con) * (size + con));
+		add += *(a + con * size + con);
 	}
 	for (con2 = 0; con2 < size; con2++)
 	{
-		add2 += *((a + con2) * (size + (size - 1 - con2)));
+		add2 += *(a + con2 * size + (size - 1 - con2));
 	}
 	printf("%d, %d\n", add, add2);
 }
