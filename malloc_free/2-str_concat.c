570b0c74c9eb15758e09539de59160482cd585c2
Mtiass <MDLSproff@gmail.com>
2023-11-02 10:44:17 -0700

    malloc, free - 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,9 +19,7 @@ char *str_concat(char *s1, char *s2)
 		strcpy(s3, s1);
 	else if (s2 != NULL)
 		strcat(s3, s2);
-	else if (s1 == NULL && s2 != NULL)
+	if (s1 == NULL && s2 != NULL)
 		strcpy(s3, s2);
-	else if (s1 == NULL && s2 == NULL)
-		s3 = '\0';
 	return (s3);
 }
