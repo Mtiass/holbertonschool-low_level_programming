27bc5fa2c2bd4c22884c0893a35f591d0d75900b
Mtiass <MDLSproff@gmail.com>
2023-11-02 07:03:15 -0700

    malloc, free - 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,11 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+		s2 = "";
 	s3 = malloc((strlen(s1) + strlen(s2)) * sizeof(char) + 1);
 	if (s3 == NULL)
 		return (NULL);
