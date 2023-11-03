52e4ac32e1b12a700849659c0d2e18d2d0fd03ba
Mtiass <MDLSproff@gmail.com>
2023-11-03 06:58:10 -0700

    More, malloc, free - 2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Entry point
+ * Description:function that allocates memory for an array, using malloc.
+ * @nmemb: number of elements of the array.
+ * @size: size in bytes of each element.
+ * Return:void pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *intar;
+
+	if ((nmemb || size) == 0)
+		return (NULL);
+	intar = malloc(nmemb * size);
+	if (intar == NULL)
+	{
+		free(intar);
+		return (NULL);
+	}
+	return (intar);
+}
