a4a757ce836f2c1833a938d8903cf4d5be58b64f
Mtiass <MDLSproff@gmail.com>
2023-11-03 06:22:23 -0700

    More malloc, free - 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - Entry point
+ * Description:function that creates an array of integers.
+ * @min: integer which initialiazes the array.
+ * @max: integer where the array finishes.
+ * Return: a pointer to an int array.
+ */
+int *array_range(int min, int max)
+{
+	int *intar;
+	int con = min;
+
+	intar = malloc(max * sizeof(int));
+	if (min > max || intar == NULL)
+	{
+		free(intar);
+		return (NULL);
+	}
+	while (con <= max)
+	{
+		intar[con] = con;
+		con++;
+	}
+	return (intar);
+}
