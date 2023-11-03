41964baf06f9a81bef66fd738514d7dd3e2cdf75
Mtiass <MDLSproff@gmail.com>
2023-11-03 12:20:12 -0700

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s3 = malloc(strlen(s1) + strlen(s2));
+	s3 = malloc(strlen(s1) + n + 1);
 	if (s3 == NULL)
 	{
 		return (NULL);
@@ -31,16 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		strcat(s3, s2);
 	else
 	{
-		s4 = malloc(n);
-		if (s4 == NULL)
-		{
-			free(s3);
-			return (NULL);
-		}
-		strncpy(s4, s2, n);
-		s4[n] = '\0';
-		strcat(s3, s4);
-		free(s4);
+		strncat(s3, s2, n);
 	}
 	return (s3);
 }
