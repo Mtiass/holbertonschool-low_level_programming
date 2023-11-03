a02bda0502c9c086cd05096c024515069eaa51e0
Mtiass <MDLSproff@gmail.com>
2023-11-03 06:32:56 -0700

    More malloc, free - 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 	int *intar;
 	int con = min;
 
-	intar = malloc((max - min)  * sizeof(int));
+	intar = malloc((max - min + 1)  * sizeof(int));
 	if (min > max || intar == NULL)
 	{
 		free(intar);
