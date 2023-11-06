505e2aa083f1e3dd6d11bf3382c1c40b592de2dc
Mtiass <MDLSproff@gmail.com>
2023-11-06 13:00:26 -0800

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Entry point
+ * Description:function that frees dogs.
+ * @d: dog to be freed.
+ * Return:
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
