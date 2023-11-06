9c5c36eeef70e402c3eafdf1c00e62b6463ceb53
Mtiass <MDLSproff@gmail.com>
2023-11-06 03:11:42 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _dog_h
 #define _dog_h
-
+/**
+ * struct dog - dog's information.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ *
+ * Description: dog's name, age and owner information.
+ */
 typedef struct dog
 {
 	char *name;
