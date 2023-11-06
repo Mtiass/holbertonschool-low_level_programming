09baffdf4e1f717415e2ad0128bb562e6c0e3028
Mtiass <MDLSproff@gmail.com>
2023-11-06 12:21:06 -0800

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * dog_t - Entry point
+ * Description:function that creates a new dog.
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: pointer to new_dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *copyn = strdup(name);
+        char *copyo = strdup(owner);
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	
+	if (new_dog != NULL)
+		return (new_dog);
+	else
+		return (NULL);
+}
