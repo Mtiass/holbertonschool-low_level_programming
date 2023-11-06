d07e5a9632e169f591baa88398d1b43acb30297c
Mtiass <MDLSproff@gmail.com>
2023-11-06 04:25:55 -0800

    1-init_dog.c

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -3,7 +3,6 @@
 
 int _putchar(char c);
 struct dog;
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
