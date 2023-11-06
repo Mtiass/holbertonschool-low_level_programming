aaef571c807db45301578219cb07bd1d1dd479c8
Mtiass <MDLSproff@gmail.com>
2023-11-06 11:37:51 -0800

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,13 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d.name == NULL)
-			d.name = (nil); 
-		if (d.age == NULL)
-			d.age = (nil);
-		if (d.owner == NULL)
-			d.owner = (nil);
-		printf("Name: %s\n Age: %f\n Owner: %s", d.name, d.age, d.owner);
+		if (d->name == NULL)
+			d->name = "(nil)"; 
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+		printf("Name: %s\n Age: %f\n Owner: %s", d->name, (*d).age, d->owner);
 	}
 }
 		
