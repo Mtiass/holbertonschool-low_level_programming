9d255713999f9e98867dc5d50ad62c995eb6fe96
Mtiass <MDLSproff@gmail.com>
2023-11-09 13:59:03 -0800

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Entry point
+ * Description:function that executes a function given as a parameter on each
+ * element of an array.
+ * @array:pointer type integer(array of integers)
+ * @size: size of the array.
+ * @action: pointer to a function.
+ * Return:
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t con;
+
+	if (action != NULL)
+	{
+		for (con = 0; con < size; con++)
+		{
+			action(array[con]);
+		}
+	}
+}
