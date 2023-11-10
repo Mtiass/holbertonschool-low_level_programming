fca524fcbb52c401c6c98a2a4ec90a92179a9068
Mtiass <MDLSproff@gmail.com>
2023-11-09 20:22:36 -0800

    Calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,16 +7,16 @@
  * Return:a pointer to the function that corresponds to the operator
  * given as a parameter.
  */
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-};
 int (*get_op_func(char *s))(int, int)
 {
+	static op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i = 0;
 
 	while (ops[i].op != NULL)
