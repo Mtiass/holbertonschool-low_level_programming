0d74ca4e6f3f68ed31bca6c27baf6eafac93e701
Mtiass <MDLSproff@gmail.com>
2023-11-09 19:44:37 -0800

    Calculator

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -13,17 +13,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-/**
- * ops - array op_t(struct op)
- */
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-};
 int i;
 int op_add(int a, int b);
 int op_sub(int a, int b);
