96cefc0698345bf296e1e0e2fbb15a205c09c207
Mtiass <MDLSproff@gmail.com>
2023-11-11 09:00:29 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -30,15 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	if (new_dog->name == NULL || new_dog->owner == NULL)
-	{
-		if (new_dog->owner == NULL)
-		free(new_dog->owner);
-		if (new_dog->name == NULL)
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
 	new_dog->name = copyn;
 	new_dog->age = age;
 	new_dog->owner = copyo;
