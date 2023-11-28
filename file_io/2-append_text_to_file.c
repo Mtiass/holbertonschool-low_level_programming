6b5d63620725f66903a50f9e24c5b67a5733c4b8
Mtiass <MDLSproff@gmail.com>
2023-11-28 15:59:17 -0800

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	if (fd == -1)
 		return (-1);
 	bytwr = write(fd, text_content, strlen(text_content));
