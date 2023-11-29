e7052fabc841d6c477f4ad5d6f5e48dd4eebfc2c
Mtiass <MDLSproff@gmail.com>
2023-11-28 16:13:21 -0800

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	ssize_t bytwr; /*Bytes written*/
 
 	if (fd == -1)
