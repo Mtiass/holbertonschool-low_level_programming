ab43e6561f31e29d3d2f7846972230caaa1cc270
Mtiass <MDLSproff@gmail.com>
2023-12-01 09:37:31 -0800

    4-hash_table_get.c fixes

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 	hash_node_t *acnode;
 
-	if (ht == NULL | key == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	i = key_index((unsigned char *)key, ht->size);
